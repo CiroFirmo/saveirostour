4ce0dbf9ae323b969b287e04d1283f775b4a63da
Ciro Firmo Faro de Souza <cirofirmofaro@gmail.com>
2025-03-24 05:24:49 -0300

    Ajusta importação da logo

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
+import logo from '../../../public/images/logo.png'
 
 export default function Logo() {
   return (
     <div className="flex items-center gap-2">
       <Image
-        src="https://saveiros.com.br/home/wp-content/uploads/2023/02/cropped-LOGO_SAVEIROS.png"
+        src={logo}
         alt="Saveiros Logo"
         width={100}
         height={35}
